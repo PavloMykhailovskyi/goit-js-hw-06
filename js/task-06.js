fea442871c805856f18c59e3281289dc5fc71159
PavloMykhailovskyi <100272784+PavloMykhailovskyi@users.noreply.github.com>
2022-08-09 22:02:43 +0300

    fix task 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,13 @@ const textInput = document.querySelector("#validation-input");
 const inputAttribute = textInput.getAttribute("data-length");
 
 const onBlur = (event) => {
-  event.currentTarget.value.length === Number(inputAttribute)
-    ? textInput.classList.add("valid")
-    : textInput.classList.add("invalid");
+  if (event.currentTarget.value.length === Number(inputAttribute)) {
+    textInput.classList.remove("invalid");
+    textInput.classList.add("valid");
+    return;
+  }
+  textInput.classList.remove("valid");
+  textInput.classList.add("invalid");
 };
 
 textInput.addEventListener("blur", onBlur);
